282d6d60a7cf08d394b94068dd5a3384301f5a1e
Dylan.Xie <wenchuan.xie@gmail.com>
2023-05-30 16:18:19 +0800

    first commit

---
+++ demo.R
@@ -0,0 +1,15 @@
+
+
+install.packages("../sequenza_3.0.0.tar.gz",repos=NULL,type='source')
+install.packages("../EstimateClonality_1.0.tar.gz",repos=NULL,type='source')
+
+library(sequenza) # 3.0
+library(EstimateClonality) # 1.0
+# 报错: could not find function "types.matrix"
+# 经查: types.matrix 函数名在3.0的sequenza包中为baf.types.matrix
+# 解决: 修改main.funxtions.r文件中的earlyORlate.strict函数
+clonality.estimation(mutation.table.loc="./data/Input/BLCA.mutation.table.txt" ,
+                     seg.mat.loc="./data/Input/tcga.blca.seg.hg19.rdata" ,
+                     data.type='TCGA_BLCA' ,
+                     TCGA.barcode="TCGA-BT-A42C" ,
+                     ANALYSIS.DIR="demo/")
